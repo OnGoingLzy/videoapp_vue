1b4221307e42c9a21fba63e84a4364788dada973
lllzy <2231003533@qq.com>
2022-07-10 21:35:14 +0800

    7/10little change

--- src/components/adminComponents/adminVideoAudit.vue
+++ src/components/adminComponents/adminVideoAudit.vue
@@ -18,20 +18,22 @@
         </div>
         <van-button  type="primary" size="small" class="video-audit-button" color="#8470FF" @click="videVideo(video)">查看</van-button>
         <van-button type="primary" size="small" class="video-audit-button" color="red">驳回</van-button>
-        <van-button type="primary" size="small" class="video-audit-button">通过</van-button>
+        <van-button type="primary" size="small" class="video-audit-button" @click="passAudit(video.videoid)">通过</van-button>
       </div>
     </van-list>
 
     <van-popup v-model="view" @close="view = false" :style="{height: '50%',width:'80%',borderRadius:'5px'}">
+      <van-icon name="shenhe" size="50px" style="left: 140px;padding: 8px" color="#5470ff"></van-icon>
       <div class="img-box">
-        <div class="mg-box-text">
+        <div class="img-box-text">
           <span style="font-size: 14px;color: #4b4b4b;">视频封面</span>
         </div>
         <van-image
             :src="'http://localhost:9000/videoCover/'+video.coverpath"
             width="180px"
             height="120px"
-            style="padding-left: 40px"
+            @click="showvideoplay=true"
+
         />
       </div>
       <van-field
@@ -49,12 +51,35 @@
           maxlength="50"
           show-word-limit
       />
-      <div class="category-box">
 
+      <div class="img-box">
+        <div class="img-box-text">
+          <span style="font-size: 14px;color: #4b4b4b;">视频标签</span>
+        </div>
+        <div>
+          <van-tag round type="primary" v-for="(tag,index) of taglist" style="margin: 5px;" color="darkgrey">{{tag}}</van-tag>
+        </div>
       </div>
-      <div class="tag-box">
 
-      </div>
+      <van-dialog
+          style="width:100%;border-radius:0;height:200px"
+          theme="default"
+          v-model="showvideoplay"
+          :show-cancel-button="false"
+          :show-confirm-button="false"
+          closeOnClickOverlay
+          onclose="closeRunVideo"
+      >
+        <video
+            controls
+            preload="auto"
+            style="width:100%;height:200px;object-fit: contain;"
+            :src="video.videopath"
+            v-show="video.videopath"
+            id="prevideo"
+        ></video>
+      </van-dialog>
+
     </van-popup>
   </div>
 </template>
@@ -71,10 +96,20 @@ export default {
       loading: false,
       finished: false,
       view: false,
+      taglist: [],
+      showvideoplay: false,
       video: [],
     }
   },
   methods:{
+    async passAudit(videoid) {
+      let formData = new FormData
+      formData.append("videoid")
+      await this.$http.post("passVideoAudit", formData)
+    },
+    closeRunVideo(){
+      this.showvideoplay=false
+    },
     clickBack(){
       this.$router.back()
     },
@@ -93,6 +128,8 @@ export default {
     },
     videVideo(video){
       this.video = video
+      console.log(video.tag)
+      this.taglist = video.tag.split(',');
       this.view = true
     }
   }
